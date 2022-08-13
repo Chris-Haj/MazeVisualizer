57cb93548c435591526564b2f068a3943489d972
Chris-Haj <chrishaj1191@gmail.com>
2022-08-13 20:42:41 +0300

    Collisions fixed.

--- MazeBuilder.py
+++ MazeBuilder.py
@@ -3,39 +3,32 @@ from chars import *
 import Maze as mz
 
 Multiplier = HEIGHT // DIM
-
-
-def PrepareCoords() -> list[25]:
-    """
-    returns a list of coordinates for the grid
-    """
-    coords = [[(j * Multiplier + 50, i * Multiplier + 50) for j in range(DIM)] for i in range(DIM)]
-    return coords
+Coords = [[(j * Multiplier + 50, i * Multiplier + 50) for j in range(DIM)] for i in range(DIM)]
+WallCoords = []
 
 
 def main() -> None:
     pg.init()
     screen = pg.display.set_mode((WIDTH + 1, HEIGHT + 1))
     puzzle = mz.Puzzle()
-    print(puzzle.start)
     start = puzzle.start[::-1]
     start = [start[i] * Multiplier + 50 for i in range(2)]
     end = puzzle.end[::-1]
     end = [end[i] * Multiplier + 50 for i in range(2)]
     player = Player(tuple(start))
     goal = Goal(tuple(end))
-    Coords = PrepareCoords()
     walls = drawWalls(Coords, puzzle.maze)
-    mainLoop(screen, player, walls, goal)
+    mainLoop(screen, player, walls, goal, start, end)
 
 
-def drawWalls(Coords: list[25], maze: list) -> list:
+def drawWalls(Coords: list, maze: list) -> list:
     cur = i = j = 0
     walls = [] * GRID
     for row in maze:
         for block in row:
             if block == 0:
                 walls.insert(cur, Wall(Coords[i][j]))
+                WallCoords.insert(cur, list(Coords[i][j]))
                 cur += 1
             j += 1
         i += 1
@@ -49,21 +42,21 @@ def drawGrid(screen: pg.display) -> None:
         pg.draw.line(screen, color='white', start_pos=(0, i), end_pos=(HEIGHT, i))
 
 
-def mainLoop(screen: pg.display, player: Player, walls, goal) -> None:
+def mainLoop(screen: pg.display, player: Player, walls, goal, curPos, end) -> None:
     running = True
     clock = pg.time.Clock()
-    FPS = 15
+    FPS = 10
     group = pg.sprite.Group()
     allBlocks = pg.sprite.Group()
     for wall in walls:
         group.add(wall)
         allBlocks.add(wall)
-    allBlocks.add(player)
     allBlocks.add(goal)
-
-    while running:
+    allBlocks.add(player)
+    frames = 0
+    while  running:
         clock.tick(FPS)
-
+        frames+=1
         for event in pg.event.get():
 
             pressed = pg.key.get_pressed()
@@ -79,11 +72,7 @@ def mainLoop(screen: pg.display, player: Player, walls, goal) -> None:
         drawGrid(screen)
         for block in allBlocks:
             screen.blit(block.surf, block.rect)
-
-        if not pg.sprite.spritecollideany(player, group):
-            player.update(pressed)
-        else:
-            curPos = player
+        player.update(pressed,curPos,WallCoords)
         pg.display.update()
 
 
--- chars.py
+++ chars.py
@@ -1,5 +1,6 @@
 from pygame import sprite, Surface
 import random as ra
+from Maze import N
 from pygame.locals import (
     KEYDOWN,
     K_ESCAPE,
@@ -11,8 +12,9 @@ from pygame.locals import (
     QUIT
 )
 
-WIDTH, HEIGHT = 500, 500
-DIM = 5
+LENGTH = 100
+DIM = N
+WIDTH, HEIGHT = DIM * LENGTH, DIM * LENGTH
 GRID = DIM * DIM
 SIZE_OF_OBJECT = (WIDTH // DIM, HEIGHT // DIM)
 
@@ -31,26 +33,30 @@ class Player(sprite.Sprite):
         self.surf = Surface(tuple(SIZE_OF_OBJECT[i] - 4 for i in range(2)))
         self.surf.fill(color='white')
         self.rect = self.surf.get_rect(center=position)
+        self.jump = SIZE_OF_OBJECT[0]
 
-    def update(self, pressed) -> None:
-        if pressed[K_UP]:
-            self.rect.move_ip(0, SIZE_OF_OBJECT[0])
-        if pressed[K_DOWN]:
-            self.rect.move_ip(0, SIZE_OF_OBJECT[0])
-        if pressed[K_LEFT]:
-            self.rect.move_ip(-SIZE_OF_OBJECT[0], 0)
-        if pressed[K_RIGHT]:
-            self.rect.move_ip(SIZE_OF_OBJECT[0], 0)
-
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-        if self.rect.top < 0:
-            self.rect.top = 0
-        if self.rect.bottom > HEIGHT:
-            self.rect.bottom = HEIGHT
-
+    def update(self, pressed, curPos,walls) -> None:
+        nextPos = curPos[::]
+        if pressed[K_UP] and (curPos[1]-self.jump) not in walls and curPos[1] != self.jump//2:
+            nextPos[1] -= self.jump
+            if nextPos not in walls:
+                self.rect.move_ip(0, -self.jump)
+                curPos[1] -= self.jump
+        elif pressed[K_DOWN] and (curPos[1] + self.jump) not in walls and curPos[1] != HEIGHT - self.jump//2:
+            nextPos[1] += self.jump
+            if nextPos not in walls:
+                curPos[1] += self.jump
+                self.rect.move_ip(0, self.jump)
+        elif pressed[K_LEFT] and (curPos[0] - self.jump) not in walls and curPos[0] != self.jump//2:
+            nextPos[0] -= self.jump
+            if nextPos not in walls:
+                curPos[0] -= self.jump
+                self.rect.move_ip(-self.jump, 0)
+        elif pressed[K_RIGHT] and (curPos[0] + self.jump) not in walls and curPos[0] != WIDTH - self.jump//2:
+            nextPos[0] += self.jump
+            if nextPos not in walls:
+                curPos[0] += self.jump
+                self.rect.move_ip(self.jump, 0)
 
 class Goal(sprite.Sprite):
     def __init__(self, position):
