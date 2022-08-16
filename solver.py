5f13d625c17ba237e3557e1d21f12786f28f7aba
Chris-Haj <chris1191@gmail.com>
2022-08-17 00:30:30 +0300

    Generator and game play finitooo

--- solver.py
+++ solver.py
@@ -1,18 +1,5 @@
 N = 6
 
-
-class Puzzle:
-    def __init__(self):
-        self.start = [0, 0]
-        self.end = [5, 0]
-        self.maze = [[1, 1, 1, 1, 1, 0],
-                     [0, 0, 0, 0, 1, 1],
-                     [1, 1, 1, 0, 0, 1],
-                     [1, 0, 1, 0, 1, 1],
-                     [1, 0, 1, 0, 1, 0],
-                     [1, 0, 1, 1, 1, 0]]
-
-
 def isSafe(maze, x, y) -> bool:
     if 0 <= x < N and 0 <= y < N and maze[x][y] == 1:
         return True
